50dcfe5967dbf59d65a9b0ebe0b4bff642f421d9
David Mikalova <324548+dmikalova@users.noreply.github.com>
2021-11-12 01:51:50 -0800

    Linted repo

--- modules/javascript/terraform.tf
+++ modules/javascript/terraform.tf
@@ -1,14 +1,4 @@
 terraform {
-  experiments = [module_variable_optional_attrs]
-  required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "~> 2"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "~> 3"
-    }
-  }
+  experiments      = [module_variable_optional_attrs]
   required_version = "~> 1"
 }
--- modules/workflows/test-build-deploy/terraform.tf
+++ modules/workflows/test-build-deploy/terraform.tf
@@ -1,4 +1,14 @@
 terraform {
-  experiments      = [module_variable_optional_attrs]
+  experiments = [module_variable_optional_attrs]
+  required_providers {
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3"
+    }
+  }
   required_version = "~> 1"
 }
